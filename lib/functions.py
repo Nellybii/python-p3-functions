85ea2f27a2c8416ee35f7ef6dda888717d9a5c1f
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-11-20 21:12:17 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,29 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+greet_programmer()
+
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet("Doe")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default("John")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+result = add(45, 55)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+
+halved_value = halve(100)
+print(halved_value)
+
